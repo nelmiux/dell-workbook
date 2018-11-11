a573274e9679a9b86f3a2c453429fd75fa6faa04
Perera <Nelma_Perera@Dell.com>
2018-11-10 23:25:01 -0600

    05grid finished

--- 07javascripting/introduction.js
+++ 07javascripting/introduction.js
@@ -1,1 +1,9 @@
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// let filtered = numbers.filter(n => n % 2 === 0);
+
+// console.log(filtered);
+
+let food = ['apple', 'pizza', 'pear'];
+
+console.log(food[1]);
