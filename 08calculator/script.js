830fc02bca22f6e000ade447ddb4ac0ba1a45aaf
Perera <Nelma_Perera@Dell.com>
2018-11-30 15:12:10 -0600

    calculator finished

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -1,75 +1,24 @@
 'use strict';
 
-let accumulator = null;
-let pendingBinaryOperation = null;
+document.addEventListener('DOMContentLoaded', () => {
+  let ops = {
+    '=': () => eval(document.querySelector('#results').value),
+    '+/-': () => -1 * parseInt(eval(document.querySelector('#results').value), 10),
+    Delete: () => document.querySelector('#results').value.slice(0, -1),
+    Clear: () => ''
+  };
+  document.querySelectorAll('.button').forEach(numb => {
+    numb.addEventListener('click', ev => {
+      const text = ev.target.innerHTML;
+      document.querySelector('#results').value += text;
+    });
+  });
 
-let ops = {
-  '=': equals,
-  '+/-': unaryOperation(x => -x),
-  '+': binaryOperation(a, b, '+'),
-  '-': binaryOperation(a, b, '-'),
-  '*': binaryOperation(a, b, '*'),
-  '/': binaryOperation(a, b, '/')
-};
-
-const equals = () =>
-  (document.querySelector('#results').value = eval(document.querySelector('#results').value));
-
-function unaryOperation(operator) {
-  if (accumulator) {
-    accumulator = operator(accumulator);
-  }
-}
-
-function binaryOperation(a, b, op) {
-  if (accumulator) {
-    pendingBinaryOperation = eval(`${accumulator}${op}${b}`);
-    accumulator = null;
-    return;
-  }
-  if (pendingBinaryOperation) {
-    eval(`${pendingBinaryOperation}${op}${b}`);
-  }
-}
-
-function constant(a) {
-  accumulator = parseInt(a, 10);
-}
-
-function performOperation(op) {
-  ops[op];
-}
-
-function doOp(num) {
-  const b = parseInt(num, 10);
-  let a = parseInt(document.querySelector('#results').value, 10);
-  a = a ? a : 0;
-  document.querySelector('#results').value = ops[op](a, b);
-}
-
-function binaryOp(o) {
-  operation(o);
-  setResult(o);
-}
-
-function unaryOp(o, num) {
-  operation(o);
-  doOp(num);
-}
-
-function operation(o) {
-  op = o;
-}
-
-function setResult(o) {
-  document.querySelector('#results').value += o;
-}
-
-function deleteLast() {
-  let current = document.querySelector('#results').value;
-  document.querySelector('#results').value = current.slice(0, -1);
-}
-
-function clearResults() {
-  document.querySelector('#results').value = '';
-}
+  document.querySelectorAll('.operator').forEach(oper => {
+    oper.addEventListener('click', ev => {
+      const op = ev.target.innerHTML;
+      document.querySelector('#results').value = ops[op]();
+      console.log(op);
+    });
+  });
+});
